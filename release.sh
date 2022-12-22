35a034c2bdc2cfff89f74be39bd044ac72541f4c
Jules Tréhorel <jules.trehorel@iadvize.com>
2022-12-22 16:26:05 +0100

    (build) update release script

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ if [ -f "IAdvizeSDK.zip" ]; then
     unzip -q IAdvizeSDK.zip -d tmp
 
     echo -e "\033[1;42m => Extracting version name \033[0m"
-    versionName=$(grep "spec.version" tmp/iAdvize.podspec -m 1 | sed "s/  spec.version                  = '\(.*\)'/\1/")
+    versionName=$(grep '"version":' tmp/plugin/package.json -m 1 | sed 's/  "version": "\(.*\)",/\1/')
 
     echo -e "\033[1;31m WARNING - You are about to push release ${versionName} to the public demo repository. \033[0m"
     echo -e "\033[1;31m Proceed ? [y/n] \033[0m"
@@ -21,21 +21,13 @@ if [ -f "IAdvizeSDK.zip" ]; then
             exit 0
     esac
 
-    echo -e "\033[1;42m => Updating framework \033[0m"
-    rm -rf IAdvizeConversationSDK.xcframework
-    mv tmp/IAdvizeConversationSDK.xcframework IAdvizeConversationSDK.xcframework
+    echo -e "\033[1;42m => Updating package.json to target latest SDK\033[0m"
+    sed -i '' "s/\"@iadvize-oss\/iadvize-react-native-sdk\": \"^\(.*\)\",/\"@iadvize-oss\/iadvize-react-native-sdk\": \"^${versionName}\",/" package.json
 
-    echo -e "\033[1;42m => Updating podspec \033[0m"
-    mv tmp/iAdvize.podspec iAdvize.podspec
-
-    echo -e "\033[1;42m => Updating CHANGELOG, UPGRADING & README. \033[0m"
+    echo -e "\033[1;42m => Updating CHANGELOG, UPGRADING & README \033[0m"
     mv tmp/CHANGELOG.md CHANGELOG.md
     mv tmp/UPGRADING.md UPGRADING.md
     mv tmp/README.md README.md
-
-    echo -e "\033[1;42m => Updating documentation \033[0m"
-    rm -rf docs
-    mv tmp/docs docs
 
     echo -e "\033[1;42m => Committing/pushing version update \033[0m"
     git add --all
@@ -44,9 +36,8 @@ if [ -f "IAdvizeSDK.zip" ]; then
     git push origin master --tags
 
     echo -e "\033[1;42m => Release ${versionName} is now public! This is what remains for you to do: \033[0m"
-    echo -e "\033[1;95m - Create a github release from tag ${versionName} : https://github.com/iadvize/iadvize-ios-sdk/releases/new \033[0m"
-    echo -e "\033[1;95m - Fill description with changelog info and add IAdvizeSDK.zip as binary to the release \033[0m"
-    echo -e "\033[1;95m - Deploy the pod to cocoapods (pod trunk push iAdvize.podspec --allow-warnings --skip-import-validation)\033[0m"
+    echo -e "\033[1;95m - Create a github release from tag ${versionName} : https://github.com/iadvize/iadvize-react-native-sdk/releases/new \033[0m"
+    echo -e "\033[1;95m - Fill description with changelog info \033[0m"
 
     rm -R tmp
     exit 0
