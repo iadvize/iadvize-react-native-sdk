ffb27f8e5adf35d5c564d5b30ba4588136de4648
Jules Tréhorel <jules.trehorel@iadvize.com>
2025-07-18 15:31:15 +0200

    (doc) add Expo integration doc

---
+++ expo-integration/expo-integration.md
@@ -0,0 +1,73 @@
+
+### 1. Create the Expo project
+```
+npx create-expo-app@latest expo-test-idz
+cd expo-test-idz
+npx expo install --fix
+```
+
+### 2. Add iAdvize Mobile SDK & expo-build-properties dependencies
+
+```
+npx expo install expo-build-properties
+npx expo install @iadvize-oss/iadvize-react-native-sdk@4.3.3-kotlin1
+```
+
+### 3. Configure iAdvize SDK properties via `expo-build-properties` in `app.json`
+
+```
+"plugins": [
+  ...
+  ,
+  [
+    "expo-build-properties",
+    {
+      "android": {
+        "compileSdkVersion": 35,
+        "targetSdkVersion": 35,
+        "buildToolsVersion": "35.0.0",
+        "minSdkVersion": 24
+      },
+      "ios": {
+        "deploymentTarget": "15.1"
+      }
+    }
+  ],
+  "./iadvize.config.js"
+],
+```
+
+### 4. Generate native code
+
+```
+npx expo prebuild --clean
+```
+
+### 5. Activate iAdvize Mobile SDK
+
+```
+<Button title="Activate iAdvize SDK" onPress={() => activateSDK()} />
+```
+
+```
+const activateSDK = () => {
+  IAdvizeSDK.setLogLevel(LogLevel.VERBOSE);
+  IAdvizeSDK.setLanguage('en');
+  IAdvizeSDK.setDefaultFloatingButton(true);
+  IAdvizeSDK.setFloatingButtonPosition(25, 25);
+  IAdvizeSDK.activate(PROJECT_ID, USER_ID, null).then( success => {
+    if (success == true) {
+      console.log('iAdvize SDK activated');
+      IAdvizeSDK.activateTargetingRule(TARGETING_RULE_ID, ConversationChannel.CHAT);
+    } else {
+      console.log('Error activating iAdvize SDK');
+    }
+  });
+};
+```
+
+### 6. Run the project
+
+```
+npx expo run android
+```
---
+++ expo-integration/iadvize.config.js
@@ -0,0 +1,102 @@
+const { withAppBuildGradle, withProjectBuildGradle, withMainApplication } = require("@expo/config-plugins");
+
+function addIAdvizeRepository(inputString, language) {
+  // Insert the import statement right before the line containing " maven { url 'https://www.jitpack.io' }"
+  const lineIndex = findLineIndex(inputString, "maven { url 'https://www.jitpack.io' }");
+  const repoImport = "        maven { url 'https://raw.github.com/iadvize/iadvize-android-sdk/master' }";
+  return insertLine(inputString, `${repoImport}`, lineIndex);
+}
+
+function addCoreLibraryDesugaringActivation(inputString, language) {
+  // Add option activation at the start of android block
+  const lineIndex = findLineIndex(inputString, "android {") + 1;
+  const optionActivation = "    compileOptions {\n        coreLibraryDesugaringEnabled true\n    }";
+  return insertLine(inputString, `${optionActivation}\n`, lineIndex);
+}
+
+function addCoreLibraryDesugaringDependency(inputString, language) {
+  // Add library  at the start of dependencies block
+  const lineIndex = findLineIndex(inputString, "dependencies {") + 1;
+  const libImport = "    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.1.5'";
+  return insertLine(inputString, `${libImport}\n`, lineIndex);
+}
+
+function addIAdvizeAndroidImport(inputString, language) {
+  // Insert the import statement right before the line containing "class MainApplication"
+  const lineIndex = findLineIndex(inputString, "class MainApplication");
+  const importStatement = "import com.iadvize.conversation.sdk.IAdvizeSDK";
+
+  if (language === 'java') {
+    return insertLine(inputString, `${importStatement};\n`, lineIndex);
+  } else if (language === 'kt') {
+    return insertLine(inputString, `${importStatement}\n`, lineIndex);
+  } else {
+    throw new Error('Cannot add iAdvize Android SDK: MainApplication is neither Java nor Kotlin.');
+  }
+}
+
+function addIAdvizeAndroidInit(inputString, language) {
+  // Insert the initiate call right after the line containing "super.onCreate()"
+  const lineIndex = findLineIndex(inputString, "super.onCreate()") + 1;
+
+  if (language === 'java') {
+    const initCall = "            IAdvizeSDK.initiate(this, null);";
+    return insertLine(inputString, `\n${initCall}\n`, lineIndex);
+  } else if (language === 'kt') {
+    const initCall = "    IAdvizeSDK.initiate(this)";
+    return insertLine(inputString, `\n${initCall}\n`, lineIndex);
+  } else {
+    throw new Error('Cannot add iAdvize Android SDK: MainApplication is neither Java nor Kotlin.');
+  }
+}
+
+function insertLine(inputString, lineToInsert, lineIndex) {
+  const lines = inputString.split('\n');
+  if (lineIndex < 0 || lineIndex >= lines.length) {
+    throw new Error(`Cannot add iAdvize Android SDK: Line index ${lineIndex} is invalid.`);
+  }
+  lines.splice(lineIndex, 0, lineToInsert);
+  return lines.join('\n');
+}
+
+function findLineIndex(inputString, match) {
+  const lines = inputString.split('\n');
+  for (const [index, line] of lines.entries()) {
+    if (line.includes(match)) {
+      return index;
+    }
+  }
+  throw new Error(`Cannot add iAdvize Android SDK: Cannot find line containing ${match}.`);
+}
+
+module.exports = function withIAdvizeSdk(config) {
+  config = withProjectBuildGradle(config, (config) => {
+    // Add the iAdvize Android SDK repository
+    config.modResults.contents = addIAdvizeRepository(config.modResults.contents, config.modResults.language);
+
+    return config;
+  });
+
+  // Core desugaring
+  config = withAppBuildGradle(config, (config) => {
+    // Activate option
+    config.modResults.contents = addCoreLibraryDesugaringActivation(config.modResults.contents, config.modResults.language);
+
+    // Add dependency
+    config.modResults.contents = addCoreLibraryDesugaringDependency(config.modResults.contents, config.modResults.language);
+
+    return config;
+  });
+
+  config = withMainApplication(config, (config) => {
+    // Add the iAdvize Android SDK import statement
+    config.modResults.contents = addIAdvizeAndroidImport(config.modResults.contents, config.modResults.language);
+
+    // Add the iAdvize Android SDK initiate() call
+    config.modResults.contents = addIAdvizeAndroidInit(config.modResults.contents, config.modResults.language);
+
+    return config;
+  });
+
+  return config;
+};
